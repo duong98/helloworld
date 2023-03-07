7d12b66e35f76f5fd4e4a60bbe02fc4b32ffa9c7
duong1998pro <duongtt@magezon.com>
2023-03-07 07:58:43 +0700

    second

--- registration.php
+++ registration.php
@@ -1,4 +1,4 @@
 <?php
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magezon_HelloWorld', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Duong98_HelloWorld', __DIR__);
